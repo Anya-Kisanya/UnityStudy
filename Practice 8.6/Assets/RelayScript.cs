82b8a4f13589221824c1b84b846f9a7bb64a84c2
Anya-Kisanya <anna_lynx@list.ru>
2025-06-21 11:28:53 +0300

    same practice but updated

--- Practice 8.6/Assets/RelayScript.cs
+++ Practice 8.6/Assets/RelayScript.cs
@@ -1,61 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RelayScript : MonoBehaviour
 {
 
     [SerializeField] Transform[] Runners;
-    public Transform StartPoint;
+    [SerializeField] GameObject[] Flames;
     
-    private Transform TargetTransform;
-    private int currentTransformIndex;
+    private int currentIndex;
+    private int flameInd;
 
     public float Speed;
     public bool Go;
 
+   
+
     // Start is called before the first frame update
     void Start()
     {
-     TargetTransform = Runners[1];
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (Go == true)
-        {
-            Transform currentRunner = Runners[currentTransformIndex];
-
-            currentRunner.LookAt(TargetTransform);
-            currentRunner.position = Vector3.MoveTowards(currentRunner.position, TargetTransform.position, Time.deltaTime * Speed);
-
-            var distance = Vector3.Distance(currentRunner.position, TargetTransform.position);
-
-            if (distance <= 1)
-
-
-                if (currentTransformIndex <= Runners.Length - 3)
-                {
-                    currentTransformIndex++;
-                    TargetTransform = Runners[currentTransformIndex + 1];
-                }
-
-                else
-                {
-                    currentTransformIndex = Runners.Length - 1;
-                    TargetTransform = StartPoint;
-                }
-
-            if (currentRunner.position == StartPoint.position)
-
-            {
-                TargetTransform = Runners[0];
-                Go = false;
-                Debug.Log("Конец эстафеты");
-            }
-            
-        }
+       if (Go == true)
+       ChangeRunner();
+           
+        
     }
+
+    public void ChangeRunner()
+    {
+
+        if (currentIndex < Runners.Length - 1)
+        {
+            Runners[currentIndex].position = Vector3.MoveTowards(Runners[currentIndex].position, Runners[currentIndex+1].position, Time.deltaTime * Speed);
+            Runners[currentIndex].LookAt(Runners[currentIndex+1]);
+            Flames[currentIndex].SetActive(true);
+
+            if (Runners[currentIndex].position == Runners[currentIndex + 1].position)
+            {
+                Flames[currentIndex].SetActive(false);
+                currentIndex++;
+                
+            }
+        }
+
+        else
+        {
+                Runners[currentIndex].position = Vector3.MoveTowards(Runners[currentIndex].position, Runners[0].position, Time.deltaTime * Speed);
+                Runners[currentIndex].LookAt(Runners[0]);
+                Flames[currentIndex].SetActive(true);
+
+            if (Runners[currentIndex].position == Runners[0].position)
+            {
+                Flames[currentIndex].SetActive(false);
+                currentIndex = 0;
+                
+            }
+        }
+
+    }
+
 }
